338902092e12cd077254f4535c949f5406c6ad84
Augusto José <ajtgalencar@gmail.com>
2017-09-13 22:22:26 -0300

    Defines the menu function

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 import arithmetic as arith
 import trigonometric as trig
+from collections import OrderedDict
 
+operations = OrderedDict([('Add', arith.addition),
+                          ('Divide', arith.division),
+                          ('Square root', arith.square_root),
+                          ('nth root', arith.nth_root)])
 
-def menu():
-    pass
+def menu(operations):
+    for i, operation in enumerate(operations):
+        print("%i - %s" % (i+1, operation))
+    option = int(input('\nChoose an operation: ')) - 1
+    option = list(operations.keys())[option]
+    print('\n' + option.upper())
+    print('\nResult:', operations[option]())
+
+if __name__ == '__main__':
+    menu(operations)
