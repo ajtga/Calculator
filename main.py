cc68acd65a0099eadf7744723fc637707c8d73c8
Ewerton <ewerton.amorim@live.com>
2017-09-19 13:18:54 -0300

    The interface code has been exploded in differents functions

--- main.py
+++ main.py
@@ -3,33 +3,58 @@ import trigonometric as trig
 from collections import OrderedDict
 
 
-def menu(operations, ans=None):
+def print_operations():
     print()
     for i, operation in enumerate(operations):
         print("%i - %s" % (i+1, operation))
     print("C - Clear\nQ - Quit")
-    option = input('\nChoose an operation: ')
-    if option.upper() == "C":
-        print("\nCleared.\n\n")
-        ans = float(input('\nInsert a number: '))
-        return menu(operations, ans)
-    if option.upper() == "Q":
-        print("Good bye!")
-        return None
-    option = list(operations.keys())[int(option) - 1]
+    
+    
+def clean_calculator():
+    print("\nCleared.\n\n")
+    ans = float(input('\nInsert a number: '))
+    return menu(operations, ans)
+
+
+def quit_calculator():
+    print("Good bye!")
+    return None
+
+
+def get_operation_by_index(index):
+    option = list(operations.keys())[index - 1]
     print('\n' + option.upper())
+    return option
+    
+
+def get_result(chosen, a):
     try:
-        result = operations[option](ans)
+        return operations[chosen](a)
     except:
         number = float(input('\nInsert a number: '))
         try:
-            result = operations[option](number)
+            return operations[chosen](number)
         except:
-            result = operations[option](ans, number)
+            return operations[chosen](a, number)
+
+    
+def print_result(result):
     if result.is_integer():
         print('\n    RESULT:', int(result))
     else:
         print('\n    RESULT:', result)
+        
+        
+def menu(operations, ans=None):
+    print_operations()
+    option = input('\nChoose an operation: ')
+    if option.upper() == "C":
+        return clean_calculator()
+    if option.upper() == "Q":
+        return quit_calculator()
+    chosen = get_operation_by_index(int(option))
+    result = get_result(chosen, ans)
+    print_result(result)
     menu(operations, result)
 
 
