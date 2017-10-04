406cc95df9f2fde96fea73dba685a9e5cc1a795c
Augusto José <ajtgalencar@gmail.com>
2017-10-04 17:15:26 -0300

    Remove redundant condition

--- main.py
+++ main.py
@@ -4,16 +4,13 @@ from collections import OrderedDict
 from interface import menu
 
 
-
-
-if __name__ == '__main__':
-    operations = OrderedDict([('Add', arith.addition),
-                              ('Subtract', arith.subtraction),
-                              ('Multiply', arith.multiplication),
-                              ('Divide', arith.division),
-                              ('Square root', arith.square_root),
-                              ('Cubic root', arith.cubic_root),
-                              ('nth root', arith.nth_root),
-                              ('nth power', arith.exponentiation)])
-    ans = float(input('\nInsert a number: '))
-    menu(operations, ans)
+operations = OrderedDict([('Add', arith.addition),
+                          ('Subtract', arith.subtraction),
+                          ('Multiply', arith.multiplication),
+                          ('Divide', arith.division),
+                          ('Square root', arith.square_root),
+                          ('Cubic root', arith.cubic_root),
+                          ('nth root', arith.nth_root),
+                          ('nth power', arith.exponentiation)])
+ans = float(input('\nInsert a number: '))
+menu(operations, ans)
