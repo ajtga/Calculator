1188fff767d1b090ede9c166bdef9ef9acc9c861
Augusto José <ajtgalencar@gmail.com>
2017-10-04 17:13:53 -0300

    Populate operations dict with new functions

--- main.py
+++ main.py
@@ -8,8 +8,12 @@ from interface import menu
 
 if __name__ == '__main__':
     operations = OrderedDict([('Add', arith.addition),
+                              ('Subtract', arith.subtraction),
+                              ('Multiply', arith.multiplication),
                               ('Divide', arith.division),
                               ('Square root', arith.square_root),
-                              ('nth root', arith.nth_root)])
+                              ('Cubic root', arith.cubic_root),
+                              ('nth root', arith.nth_root),
+                              ('nth power', arith.exponentiation)])
     ans = float(input('\nInsert a number: '))
     menu(operations, ans)
